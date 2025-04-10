62d95bed59f29e0ddf7bda8e2bd6e514fe20f3d1
rohitr-vl <rohitr@vedlogic.com>
2025-04-10 06:10:06 +0530

    initial commit, static frontend

---
+++ front-end/cmd/web/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"embed"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		render(w, "test.page.gohtml")
+	})
+
+	fmt.Println("Starting frond end service on Port 80")
+	err := http.ListenAndServe(":80", nil)
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+//go:embed templates
+var templateFS embed.FS
+
+func render(w http.ResponseWriter, t string) {
+	partials := []string{
+		"./cmd/web/templates/base.layout.gohtml",
+		"./cmd/web/templates/header.partial.gohtml",
+		"./cmd/web/templates/footer.partial.gohtml",
+	}
+	var templateSlice []string
+	templateSlice = append(templateSlice, fmt.Sprintf("templates/%s", t))
+
+	for _, x := range partials {
+		templateSlice = append(templateSlice, x)
+	}
+
+	// parse the templates
+	//tmpl, err := template.ParseFiles(templateSlice...)
+	tmpl, err := template.ParseFS(templateFS, templateSlice...)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// execute the template
+	if err := tmpl.Execute(w, nil); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
